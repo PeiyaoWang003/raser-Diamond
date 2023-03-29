d8224923b07badd4549a12befce5fb494e440bf6
Songting Jiang <435053757@qq.com>
2023-03-29 03:32:05 +0000

    Update plot-EH67-sigma.py

--- python/plot-EH67-sigma.py
+++ python/plot-EH67-sigma.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
 import ROOT
 import math
 from array import array
@@ -57,7 +60,7 @@ mg.Add(g_6)
 #g_2.SetLineStyle(ROOT.kDashed)
 
 #g_2.GetXaxis().SetLimits(-399.9,0)
-mg.GetXaxis().SetRangeUser(-5,805)
+mg.GetXaxis().SetRangeUser(-805,5)
 mg.GetXaxis().SetTitle("Reverse Voltage [V]")
 mg.GetXaxis().CenterTitle()
 mg.GetXaxis().SetTitleOffset(1.4)
@@ -91,4 +94,4 @@ c.SetLogy()
 mg.Draw("ALP")
 legend.Draw("SAME")
 
-c.SaveAs("./EH67-sigma.pdf")
+c.SaveAs("./output/devsim/EH67-sigma.pdf")
