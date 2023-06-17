fae169b0180ef7527e292972f31561d05749de63
zhang jun rui <1031721028@qq.com>
2023-06-17 22:45:44 +0800

    77777777

--- raser/setting.py
+++ raser/setting.py
@@ -155,16 +155,6 @@ class Setting:
         if "Carrier" in self.det_model:
             detector.update({'doping_cpp':p['doping_cpp']})
 
-<<<<<<< HEAD
-        """
-        if "trapping_time" in p:
-            detector['trapping_time']=p['trapping_time']
-        """
-
-        if "Irradiation" in p:
-            detector['Irradiation']=p['Irradiation']
-            
-=======
      
         """
         if "trapping_time" in p:
@@ -175,7 +165,6 @@ class Setting:
             detector['Irradiation']=p['Irradiation']
         
 
->>>>>>> 2c1d435fc83c3d765280655f7e84d2fd864c5ccf
         return detector
 
     def electron_custom(self,electrodes):
