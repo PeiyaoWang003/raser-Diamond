121688b2651e3d34c6c77d8279a0c01d2e0b8232
heye <heye@ihep.ac.cn>
2023-10-16 16:57:47 +0800

    raser particle FLM_v1

--- raser/particle/__init__.py
+++ raser/particle/__init__.py
@@ -4,5 +4,9 @@ def main(args):
     if label == 'temperature':
         from . import cal_temp
         cal_temp.main()
+
+    if label == 'FLM_v1':
+        from . import FLM
+        FLM.main()
     else:
         raise NameError(label)
