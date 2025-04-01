9ba467cfc495c2b1aa1b8f3a8a17b06a640bb222
fuchenxi <1256257282@qq.com>
2025-04-01 13:29:40 +0800

    调整配置文件位置

---
+++ cfg/setup_lxlogin.sh
@@ -0,0 +1,65 @@
+# Setup raser environment     
+# Author SHI Xin <shixin@ihep.ac.cn>  
+# Created [2023-08-31 Thu 08:36] 
+
+if [ -z "$PS1" ]; then
+    echo "Setting up raser ..."
+fi
+
+dir_raser=$(cd $(dirname $(dirname $BASH_SOURCE[0])) && pwd)
+dir_geant4_data=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02/data
+GEANT4_INSTALL=/cvmfs/common.ihep.ac.cn/software/geant4/10.7.p02
+
+cfg_env=$dir_raser/cfg/env
+rm -f $cfg_env
+cat << EOF >> $cfg_env
+# PATH 
+PATH=/cvmfs/common.ihep.ac.cn/software/hepjob/bin:/bin:\$PATH
+
+# ROOT 
+ROOTSYS=/usr/local/share/root_install
+
+# Geant4 
+GEANT4_INSTALL=$GEANT4_INSTALL/x86_64-centos7-gcc9-optdeb
+G4ENSDFSTATEDATA=$dir_geant4_data/G4ENSDFSTATE2.3
+G4PIIDATA=$dir_geant4_data/G4PII1.3
+G4INCLDATA=$dir_geant4_data/G4INCL1.0
+G4LEDATA=$dir_geant4_data/G4EMLOW7.13
+G4PARTICLEXSDATA=$dir_geant4_data/G4PARTICLEXS3.1.1
+G4NEUTRONHPDATA=$dir_geant4_data/G4NDL4.6
+G4SAIDXSDATA=$dir_geant4_data/G4SAIDDATA2.0
+G4REALSURFACEDATA=$dir_geant4_data/RealSurface2.2
+G4ABLADATA=$dir_geant4_data/G4ABLA3.1
+G4LEVELGAMMADATA=$dir_geant4_data/PhotonEvaporation5.7
+G4RADIOACTIVEDATA=$dir_geant4_data/RadioactiveDecay5.6
+
+# Python 
+PYTHONPATH=$dir_raser/raser:/usr/local/share/root_install/lib:$GEANT4_INSTALL/install/lib64/python3.6/site-packages:/usr/local/share/acts_build/python
+LD_LIBRARY_PATH=$GEANT4_INSTALL/x86_64-centos7-gcc9-optdeb/lib64:/usr/local/share/root_install/lib:/.singularity.d/libs
+
+#pyMTL3 Verilator
+PYMTL_VERILATOR_INCLUDE_DIR="/usr/local/share/verilator/include"
+EOF
+
+export PATH=/cvmfs/common.ihep.ac.cn/software/hepjob/bin:$PATH
+export IMGFILE=/afs/ihep.ac.cn/users/f/fuchenxi/img/raser-2.5.sif
+export BINDPATH=/afs,/besfs5,/cefs,/cvmfs,/etc/condor,/etc/redhat-release,/publicfs,/scratchfs,/workfs2
+# redhat for hep_job
+export RASER_SETTING_PATH=$dir_raser/setting
+
+# temporary solution for scipy import error
+export OPENBLAS_NUM_THREADS=1
+
+alias raser-shell="apptainer shell --env-file $cfg_env -B $BINDPATH $IMGFILE"
+
+raser_exec="apptainer exec --env-file $cfg_env -B $BINDPATH $IMGFILE"
+raser_python="$raser_exec python3"
+
+alias raser="$raser_python src/raser"
+alias raser-test="$raser_python -m unittest discover -v -s raser/tests"
+alias pytest="$raser_exec pytest"
+alias raser-install="$raser_exec pip install -e ."  
+
+alias drawfig="$raser_python misc/drawfig"
+alias control="$raser_python misc/control"
+alias mesh="$raser_python setting/detector"
--- setting/detector/__main__.py
+++ setting/detector/__main__.py
@@ -3,5 +3,5 @@ import subprocess
 import sys
 
 device = sys.argv[1]
-command = os.getenv("RASER_SETTING_PATH")+"/detector/"+device+".py"
+command = "./setting/detector/"+device+".py"
 subprocess.run(["python3"+' '+command], shell=True, executable='/bin/bash')
